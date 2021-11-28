d0e75abc1db597c6424d814db18f61222a8229b7
ram <meerdara@hotmail.com>
2021-11-28 00:24:22 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# FpsCpp
+
+Developed with Unreal Engine 4
---
+++ Source/FpsCpp/FPSHud.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/HUD.h"
+#include "Engine/Canvas.h"
+#include "FPSHUD.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FPSCPP_API AFPSHUD : public AHUD
+{
+	GENERATED_BODY()
+
+public:
+	// Primary draw call for the HUD.
+	virtual void DrawHUD() override;
+
+protected:
+	// This will be drawn at the center of the screen.
+	UPROPERTY(EditDefaultsOnly)
+	UTexture2D *CrosshairTexture;
+};
